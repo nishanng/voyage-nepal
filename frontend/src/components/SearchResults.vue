563962aa396aa2bcbe343e1b8c4f9f99396ae2e4
nishan gautam <nishangautam.ng@gmail.com>
2023-04-17 02:49:30 +1000

    updated searchresults

--- frontend/src/components/SearchResults.vue
+++ frontend/src/components/SearchResults.vue
@@ -1,28 +1,50 @@
 <template>
-  <div class="results">
-    <h1>Results</h1>
-    <div v-if="results">
+  <div class="search-results">
+    <SearchBox />
+    <div v-if="results" class="response-container">
+      <h3>AI Response:</h3>
       <p>{{ results }}</p>
-      <!-- Display the results in a formatted manner -->
     </div>
-    <button @click="$router.push('/')">New Search</button>
   </div>
 </template>
 
 <script>
+import SearchBox from '@/components/SearchBox.vue';
+
 export default {
-  computed: {
-    results() {
-      return this.$route.params.results;
+  components: {
+    SearchBox,
+  },
+  props: {
+    results: {
+      type: String,
+      default: '',
     },
   },
 };
 </script>
 
 <style scoped>
-.results {
+.search-results {
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100%;
+  gap: 1rem;
+}
+
+.response-container {
+  max-width: 600px;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #f5f5f5;
+}
+
+h3 {
+  font-weight: 600;
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+  color: #333;
 }
 </style>
