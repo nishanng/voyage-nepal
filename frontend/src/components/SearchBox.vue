f1c27763402c7729cd94e23992f1f2a2a5fe753a
nishan gautam <nishangautam.ng@gmail.com>
2023-04-17 17:57:10 +1000

    updated more

--- frontend/src/components/SearchBox.vue
+++ frontend/src/components/SearchBox.vue
@@ -6,41 +6,68 @@
       <div class="input-container">
         <input
           type="text"
-          v-model="query"
+          v-model="userInput"
           placeholder="Search for places, hotels, routes and more....."
-          @keyup.enter="search"
+          @keyup.enter="handleSubmit"
         />
-        <div class="search-icon" @click="search">
+        <div class="search-icon" @click="handleSubmit">
           <span>✈</span>
         </div>
       </div>
     </div>
+    <div id="chat_container" class="chat-container"></div>
   </div>
 </template>
 
 <script>
-import api from '@/api.js';
+import api from "@/api.js";
 
 export default {
   data() {
     return {
-      query: '',
+      userInput: "",
+      chatContainer: null,
     };
   },
   methods: {
-    async search() {
-      try {
-        const response = await api.search(this.query);
-        this.$emit('search-completed', response.data.text);
-      } catch (error) {
-        console.error('Error during search:', error);
+    chatStripe(sender, message, cssClass) {
+      const msgDiv = document.createElement("div");
+      msgDiv.classList.add("msg", cssClass);
+      msgDiv.innerHTML = `<span class="msg-${sender}">${message}</span>`;
+      this.chatContainer.appendChild(msgDiv);
+    },
+    loader() {
+      const loader = document.createElement("div");
+      loader.classList.add("loader");
+      this.chatContainer.appendChild(loader);
+    },
+    async handleSubmit() {
+      if (this.userInput.trim() !== "") {
+        this.chatStripe("user", this.userInput, "user-msg");
+
+        this.userInput = "";
+
+        this.loader();
+
+        try {
+          const response = await api.search(this.userInput);
+
+          this.chatContainer.removeChild(this.chatContainer.lastChild);
+
+          const aiResponse = response.data.text.trim();
+          this.chatStripe("bot", aiResponse, "bot-msg");
+        } catch (error) {
+          console.error("Error during search:", error);
+        }
       }
     },
+  },
+  mounted() {
+    this.chatContainer = document.getElementById("chat_container");
   },
 };
 </script>
 
-<!-- Original styling -->
 <style scoped>
 .search-box {
   display: flex;
@@ -92,7 +119,7 @@ input:focus {
   background-color: #3a86ff;
   border-radius: 0 4px 4px 0;
   cursor: pointer;
-  transition: background-color 0.3s ease-in-out;
+  transition: background-color 0.3  s ease-in-out;
 }
 .search-icon:hover {
   background-color: #2a63cc;
@@ -104,4 +131,52 @@ input:focus {
 button:focus {
   outline: none;
 }
+
+.chat-container {
+  margin-top: 1rem;
+  width: 100%;
+  max-width: 600px;
+  background-color: #f5f5f5;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  overflow-y: auto;
+}
+
+.msg {
+  margin: 0.5rem 0;
+}
+.msg-user, .msg-bot {
+  display: inline-block;
+  padding: 0.4rem 0.8rem;
+  border-radius: 4px;
+}
+.msg-user {
+  background-color: #3a86ff;
+  color: #fff;
+}
+.msg-bot {
+  background-color: #f0f0f0;
+  color: #333;
+}
+
+.loader {
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  border: 5px solid #f3f3f3;
+  border-top: 5px solid #3498db;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
+
